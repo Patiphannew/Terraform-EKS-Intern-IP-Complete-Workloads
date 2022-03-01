ed1cdfd624c8201ea740a8d21bf0c19775b5a1ab
Patiphan Thiansuwan <newnismokung_@hotmail.com>
2022-03-01 12:21:45 +0700

    [FIX]2

--- aws-resource/eks/output.tf
+++ aws-resource/eks/output.tf
@@ -1,3 +1,12 @@
+#EKS
 output "eks_managed_node_groups" {
   value = module.eks.eks_managed_node_groups
+}
+
+#NLB
+output "lb_dns_name" {
+  value = module.nlb.lb_dns_name
+}
+output "nlb_target_group_arns" {
+  value = module.nlb.target_group_arns[0]
 }
