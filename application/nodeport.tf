a3325fa3291ddb9fa820a6551e22f6fb66add89b
Patiphan Thiansuwan <newnismokung_@hotmail.com>
2022-03-02 15:32:37 +0700

    [UPDATE]3

--- application/nodeport.tf
+++ application/nodeport.tf
@@ -1,24 +1,24 @@
-resource "kubectl_manifest" "nodeport" {
-  depends_on = [helm_release.argocd-helm]
-  yaml_body  = <<YAML
-apiVersion: v1
-kind: Service
-metadata:
-  name: nginx-ingress
-  namespace: nginx-ingress
-spec:
-  type: NodePort
-  ports:
-    - port: 80
-      targetPort: 80
-      protocol: TCP
-      name: http
-      nodePort: 32593
-    - port: 443
-      targetPort: 443
-      protocol: TCP
-      name: https
-  selector:
-    app: nginx-ingress-nginx-ingress
-YAML
-}
+# resource "kubectl_manifest" "nodeport" {
+#   depends_on = [helm_release.argocd-helm]
+#   yaml_body  = <<YAML
+# apiVersion: v1
+# kind: Service
+# metadata:
+#   name: nginx-ingress
+#   namespace: nginx-ingress
+# spec:
+#   type: NodePort
+#   ports:
+#     - port: 80
+#       targetPort: 80
+#       protocol: TCP
+#       name: http
+#       nodePort: 32593
+#     - port: 443
+#       targetPort: 443
+#       protocol: TCP
+#       name: https
+#   selector:
+#     app: nginx-ingress-nginx-ingress
+# YAML
+# }
